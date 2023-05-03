908ee8336bc745e6b2c5f4d6bf89e1eed5bb4f81
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-05-03 17:29:33 +0100

    atualizacoes

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Login from "./components/Login";
+import Login from "./components/login/Login";
 import CustomTextInput from './components/input/CustomTextInput';
 import { CustomCheckBox } from './components/checkbox/CustomCheckbox';
 import { Colors } from './utils/Colors';
 
 export default function App() {
+  
   const [checkboxState, setCheckboxState] = useState('unchecked');
   const [textInput, setTextInput] = useState('');
 
