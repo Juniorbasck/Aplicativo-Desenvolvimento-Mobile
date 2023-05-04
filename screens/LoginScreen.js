4db88aa0b1108c43bee2d3e530f2d8b41bf16b91
Junior <94143121+Juniorbasck@users.noreply.github.com>
2023-05-04 22:04:53 +0100

    conintua pagina de ScreenLogin

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {
-    View,
-    StyleSheet,
-} from 'react-native';
+import { View, StyleSheet,Text, TextInput, Button} from 'react-native';
 import { CustomButton } from '../components/button/CustomButton';
+
 
 const LoginScreen = ({navigation}) => {
     return (
-        <View style={styles.outerContainer}>
+        <View style={estilos.outerContainer}>
+
+            <View>
+                <Text style={estilos.areaTitulo}>Meu controlo</Text> 
+                <Text style={estilos.areaTitulo2}>Financeiro</Text>
+                <Text style={estilos.areaTitulo3}>$</Text>
+            </View> 
+
+            <View>
+                <TextInput
+                style={estilos.input}
+                placeholder="E-Email ou usuário"
+                />
+            </View>
+            
+            <View>
+                <TextInput
+                style={estilos.input}
+                placeholder="Palavra-passe"
+                />
+            </View>
+
+            <View style={estilos.passaword}>
+                <Text>Esquece plavra-passe?</Text>
+            </View>
+
+            <View>
+                <Button title="Entrar"/>
+            </View>
+            <View>
+                <Button title="Acessar com a conta Google"/>
+            </View>
+
             <CustomButton
                 text={'Ir para criar conta'}
                 onPress={() => navigation.navigate('CreateAccount')}
@@ -15,16 +45,65 @@ const LoginScreen = ({navigation}) => {
                 textColor={'white'}
                 widthPercentage={61}
             />
+
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    outerContainer: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
-});
+const estilos = StyleSheet.create({
 
+            outerContainer: {
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            },
+
+            container: {
+                flexDirection: "column",
+                backgroundColor: "#D3D3D3",
+                alignItems: 'center',
+                justifyContent: 'center',
+            },
+        
+            areaTitulo: {
+                // backgroundColor: "#486D31",
+                fontSize: 30,
+                width: 200,
+                marginTop: 100,
+            }, 
+            areaTitulo2: {
+                
+                // backgroundColor: "#486D31",
+                fontSize: 30,
+                width: 200,
+            }, 
+        
+            areaTitulo3: {
+        
+                //backgroundColor: "#486D31",
+                fontSize: 30,
+                marginBottom: 60 ,
+                
+            }, 
+        
+        
+            ///froms 
+        
+            input: {
+                borderWidth: 1,
+                borderColor: '#777',
+                padding: 8,
+                margin: 10,
+                width: 300,
+                borderRadius: 5,  
+            },
+        
+            passaword: {
+                alignSelf: "flex-end",
+                marginRight: 20,
+                marginTop: 10,
+            },
+        });
+        
 export { LoginScreen };
+
