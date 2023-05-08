e8572ae0ad535ba677fa496c9db1327e63260748
Fael123Programming <rafaelfonseca1020@gmail.com>
2023-05-08 10:45:28 +0100

    added home screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {
+    ScrollView,
+    View,
+    Text,
+    StyleSheet,
+} from 'react-native';
+import { Colors } from '../utils/Colors';
+import { Fonts } from '../utils/Fonts';
+
+
+const HomeScreen = () => {
+    
+};
+
+
+const styles = StyleSheet.create({
+
+});
+
+export { HomeScreen };
--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,31 +4,39 @@ import { CustomButton } from '../components/button/CustomButton';
 import { Colors } from '../utils/Colors';
 import { Fonts } from '../utils/Fonts';
 import { CustomTextInput } from '../components/input/CustomTextInput';
+import { validateEmail, validatePassword } from '../utils/Validator';
+import { StackActions } from '@react-navigation/native';
 
-
-function validateData(email, passaword){
-
-    if(validateTextField(email) && validateTextField(passaword)){
-
-        message.header = 'Sucesso!';
-    }
+function validateData(email, password){
+    let message = {};
+    message.header = 'Sucesso';
+    // if(validateEmail(email)) {
+    //     if (validatePassword(password)) {
+    //         // Check database for the given credentials: email and password.
+    //         // If all good, log in!
+    //         message.header = 'Sucesso';
+    //     } else {
+    //         message.header = 'Credenciais Inválidas!';
+    //         message.body = 'Verifique suas credenciais e tente novamente';
+    //     }
+    // } else {
+    //     message.header = 'E-mail Inválido!';
+    //     message.body = 'Endereço de e-mail inválido!';
+    // }
+    return message;
 }
 
 const LoginScreen = ({navigation}) => {
-
     const [email, setEmail] = React.useState('');   
     const [password, setPassword] = React.useState('');
 
-
     return (
         <View style={styles.outerContainer}>
-
         <View>
             <Text style={styles.title}>Meu Controlo</Text>
             <Text style={styles.subtitle}>financeiro</Text>
         </View>
         <Text style={styles.cifrao}>$</Text>
-
             <View>
                 <TextInput
                 style={[styles.input, styles.separandoConteudos]}
@@ -37,7 +45,6 @@ const LoginScreen = ({navigation}) => {
                 placeholderTextColor="black"
                 />
             </View>
-            
             <View>
                 <TextInput
                 style={styles.input}
@@ -46,14 +53,12 @@ const LoginScreen = ({navigation}) => {
                 placeholderTextColor="black"
                 />
             </View>
-
             <View style={styles.passaword}>
                 <Text 
                     style={styles.Textpassaword} 
                     onPress={() => navigation.navigate('ForgottenPassword')}
                 >Esqueceu palavra-passe?</Text>
             </View>
-
             <View>
                 <CustomButton style={styles.buttosignIn}
                     text={'Entrar'}
@@ -62,13 +67,18 @@ const LoginScreen = ({navigation}) => {
                     widthPercentage={88}
                     padding={1}
                     onPress={() => {
-                        let res = validateData(email, password);
-                        Alert.alert(res.header, res.body);
-                        if (res.header == 'Conta criada com sucesso!') {
-                            // Save account data to database.
-                            // Log user in.
+                            let res = validateData(email, password);
+                            if (res.header == 'Sucesso') {
+                                navigation.dispatch(
+                                    StackActions.replace('Home', {
+                                        userEmail: email
+                                    })
+                                );
+                            } else {
+                                Alert.alert(res.header, res.body);
+                            }
                         }
-                    }}
+                    }
                 />
             </View>
             <View>
@@ -80,85 +90,79 @@ const LoginScreen = ({navigation}) => {
                     
                 />
             </View>
-
-            
-
             <Text style={styles.createCount} onPress={() => navigation.navigate('CreateAccount')}>
                 Ir para criar conta
             </Text> 
-
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    outerContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: "#222222",
+    },
 
-            outerContainer: {
-                flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: "#222222",
-            },
+    containerTitulo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    
+    title: {
+        fontSize: 40,
+        textAlign: 'center',
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        },
+        
+    subtitle: {
+        fontSize: 40,
+        marginLeft: 35,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        },
 
-            containerTitulo: {
-                flexDirection: 'row',
-                alignItems: 'center',
-                justifyContent: 'center',
-            },
-            
-            title: {
-                fontSize: 40,
-                textAlign: 'center',
-                color: '#FFFFFF',
-                fontWeight: 'bold',
-              },
-              
-            subtitle: {
-                fontSize: 40,
-                marginLeft: 35,
-                color: '#FFFFFF',
-                fontWeight: 'bold',
-              },
+        cifrao: {
+        fontSize: 40,
+        fontWeight: 'bold',
+        color: '#FFFFFF',
+        },
+    
+    separandoConteudos:{
+        marginTop: 80,
+    },
+    input: {
+        
+        borderWidth: 1,
+        borderColor: '#777',
+        padding: 8,
+        margin: 10,
+        width: 350,
+        borderRadius: 2,  
+        backgroundColor: "#D3D3D3",
 
-              cifrao: {
-                fontSize: 40,
-                fontWeight: 'bold',
-                color: '#FFFFFF',
-              },
-            
-            separandoConteudos:{
-                marginTop: 80,
-            },
-            input: {
-                
-                borderWidth: 1,
-                borderColor: '#777',
-                padding: 8,
-                margin: 10,
-                width: 350,
-                borderRadius: 2,  
-                backgroundColor: "#D3D3D3",
+    },
+    passaword: {
+        alignSelf: "flex-end",
+        marginRight: 20,
+        marginTop: 10,
+        marginBottom: 15,
+    },
 
-            },
-            passaword: {
-                alignSelf: "flex-end",
-                marginRight: 20,
-                marginTop: 10,
-                marginBottom: 15,
-            },
-
-            Textpassaword:{
-                textDecorationLine: 'underline',
-                color: '#FFFFFF',
-            },
-            
-            createCount: {
-                textDecorationLine: 'underline',
-                color: '#FFFFFF',
-                textAlign: 'center',
-                marginTop: 35,
-            },  
-        });
+    Textpassaword:{
+        textDecorationLine: 'underline',
+        color: '#FFFFFF',
+    },
+    
+    createCount: {
+        textDecorationLine: 'underline',
+        color: '#FFFFFF',
+        textAlign: 'center',
+        marginTop: 35,
+    },  
+});
         
 export { LoginScreen };
-
